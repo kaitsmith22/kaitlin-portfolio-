711842dd5f9ac57d87c9c3422d6e742c10a22d8f
Kaitlin Smith <kaitlin.m.smith@du.edu>
2025-04-06 16:30:43 -0400

    add next config

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/kaitlin-portfolio-"
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    distDir: "dist"
+  };
+  
+  module.exports = nextConfig;
