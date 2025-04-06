db0880a7850336d441c55cabb6a367e6ae96d8cd
Kaitlin Smith <kaitlin.m.smith@du.edu>
2025-04-06 16:44:32 -0400

    remove old config file

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/kaitlin-portfolio-"
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    distDir: "dist"
-  };
-  
-  module.exports = nextConfig;
